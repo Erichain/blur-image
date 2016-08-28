715c28e32ef541f7c94ff8cc00be35650c8fef1d
Erichain <jyamol520@gmail.com>
2016-08-28 12:51:41 +0800

    [Improve] 🌟 Complete main features.

--- src/blur-img.js
+++ src/blur-img.js
@@ -7,4 +7,30 @@
  */
 (function ( window ) {
 
+    var containers = document.getElementsByName('blur');
+
+    containers.forEach(function ( elem, index ) {
+        var thumbSrc = elem.getAttribute('data-src'),
+            lgSrc = elem.getAttribute('src'),
+            realWidth = elem.getAttribute('data-real-width'),
+            realHeight = elem.getAttribute('data-real-height');
+
+        elem.style.paddingBottom = (realHeight / realWidth) * 100 + '%';
+
+        var thumb = new Image();
+        thumb.src = thumbSrc;
+        thumb.onload = function () {
+            thumb.classList.add('thumb-loaded');
+        };
+        elem.appendChild(thumb);
+
+        var realImg = new Image();
+        realImg.src = lgSrc;
+        realImg.onload = function () {
+            realImg.classList.add('large-loaded');
+            thumb.classList.add('thumb-hidden');
+        };
+        elem.appendChild(realImg);
+    });
+
 })( window );
